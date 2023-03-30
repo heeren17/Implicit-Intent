8b014ba0f7bb408bf842e0673a18859f65673397
rolan <qyarafisa@gmail.com>
2023-03-30 10:15:26 +0700

    Implicit Intent

---
+++ app/src/main/java/com/fahmidwipamane_202102364/implicitintent/MainActivity.java
@@ -0,0 +1,21 @@
+package com.fahmidwipamane_202102364.implicitintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void tampilTelepon(View view) {
+        Intent teleponIntent = new Intent(Intent.ACTION_DIAL);
+        startActivity(teleponIntent);
+    }
+}
